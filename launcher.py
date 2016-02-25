92bfe436566d97c9e83140549f857342871c6b06
flopez <lopez.fernando.damian@gmail.com>
2016-02-25 15:00:51 -0300

    Launch also Message Router

--- launcher.py
+++ launcher.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 import argparse
 
+import sys
+
 
 class Branch:
     name = None
@@ -28,38 +30,6 @@ class VSLauncher:
                 branch.base_path = os.path.join(root_dir, directory)
                 self.branches.append(branch)
 
-    def main(self, args):
-        # Look for the branches
-        self.load_branches()
-
-        option = self.ask_for_option()
-
-        if option == 0:
-            return
-
-        conf = self.ask_for_config()
-
-        if conf == 0:
-            return
-
-        branch = self.branches[option]
-
-        print('Selected branch: ', branch.name, 'Configuration: ', 'Debug' if conf == 1 else 'Release')
-
-        env = os.environ
-        env['PATH'] += os.path.join(branch.base_path, 'SSF.Dev', 'win32' if conf == 'Debug' else 'win32.release','lib')
-
-        self.launch_vs(branch, env)
-
-    def launch_vs(self, branch, env):
-        sln_file = os.path.join(branch.base_path, 'SSF.Dev', 'SSF.FC.Solution', 'SSF.FC', 'Dev', 'SSF.FC-Global.sln')
-        vs_path = ''
-        if branch.name in self.run_in_2008:
-            self.config.get('General', 'vs2008')
-        else:
-            self.config.get('General', 'vs2013')
-        subprocess.Popen([vs_path, sln_file], env=env)
-
     @staticmethod
     def ask_for_config():
         conf = 1000
@@ -81,15 +51,61 @@ class VSLauncher:
             option = int(input('Please, select a branch: '))
         return option
 
+    def launch_vs(self, branch, env):
+        sln_file = os.path.join(branch.base_path, 'SSF.Dev', 'SSF.FC.Solution', 'SSF.FC', 'Dev', 'SSF.FC-Global.sln')
+        vs_path = ''
+        if branch.name in self.run_in_2008:
+            vs_path = self.config.get('General', 'vs2008')
+        else:
+            vs_path = self.config.get('General', 'vs2013')
+        subprocess.Popen([vs_path, sln_file], env=env)
+
+    @staticmethod
+    def launch_message_router(branch, env, conf):
+        router_exe = os.path.join(branch.base_path,
+                                  'SSF.Dev',
+                                  'win32' if conf == 1 else 'win32.release', 'bin',
+                                  'SSF.Msg.Router.D.exe' if conf == 1 else 'SSF.Msg.Router.exe'
+                                  )
+        print(router_exe)
+        subprocess.Popen([router_exe, '-debug'], env=env, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+    def main(self, args):
+        # Look for the branches
+        self.load_branches()
+
+        option = self.ask_for_option()
+
+        if option == len(self.branches):
+            return
+
+        conf = self.ask_for_config()
+
+        if conf == 0:
+            return
+
+        branch = self.branches[option]
+
+        print('Selected branch: ', branch.name, 'Configuration: ', 'Debug' if conf == 1 else 'Release')
+
+        env = os.environ
+        env['PATH'] += os.path.join(branch.base_path, 'SSF.Dev', 'win32' if conf == 1 else 'win32.release', 'lib')
+
+        if args.visual_studio:
+            self.launch_vs(branch, env)
+
+        if args.msg_router:
+            self.launch_message_router(branch, env, conf)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-vs', '--visual-studio', help='Launch VS', action='store_true')
-    parser.add_argument('-m', '--message-router', help='Run Message Router', action='store_true')
+    parser.add_argument('-mr', '--msg-router', help='Run Message Router', action='store_true')
 
     args = parser.parse_args()
 
-    if not args.spiders and not args.normalizer  and not args.eraser and not args.samleren:
+    if len(sys.argv) == 1:
         parser.print_help()
     else:
         launcher = VSLauncher()
